f357ccf848a2835591f308b0a01b15d7eae075c2
Zhiyou Liu <liuzhiyou.cs@gmail.com>
2024-04-24 12:03:54 -0600

    Update main.rs
    add multi

--- src/main.rs
+++ src/main.rs
@@ -34,10 +34,18 @@ fn main() {
         .parse()
         .expect("Failed to parse seconds");
 
+    let multi = env::args().nth(4).is_some();
+
     for p in 1..producer_count+1 {
         print!("p{}: ", p);
         for c in 1..consumer_count+1 {
-            let bandwidth = single_mpmc(p, c, seconds);
+            let bandwidth;
+            if (multi) {
+                bandwidth = multi_mpmc(p, c, seconds);
+            } else {
+                bandwidth = single_mpmc(p, c, seconds);
+            }
+
             print!("{} ", bandwidth);
         }
         println!(""); // newline
@@ -94,7 +102,7 @@ fn single_mpmc(producer_count: usize, consumer_count: usize, seconds: u64) -> us
 
     let counter = Arc::new(AtomicUsize::new(0));
     let length = Arc::new(AtomicUsize::new(0));
-    let should_exit = Arc::new(AtomicBool::new(false));
+    let should_exit: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
 
     let mut handles = vec![];
 
@@ -128,4 +136,60 @@ fn single_mpmc(producer_count: usize, consumer_count: usize, seconds: u64) -> us
     }
     
     counter.load(Ordering::Relaxed) / seconds as usize
+}
+
+
+fn multi_mpmc(producer_count: usize, consumer_count: usize, seconds: u64) -> usize {
+    let counters: Vec<_> = (0..consumer_count).map(|_| Arc::new(AtomicUsize::new(0))).collect();
+    let lengths: Vec<_> = (0..consumer_count).map(|_| Arc::new(AtomicUsize::new(0))).collect();
+    let should_exit: Arc<AtomicBool> = Arc::new(AtomicBool::new(false));
+    let channels: Vec<_> = (0..consumer_count).map(|_| channel::unbounded::<String>()).collect();
+
+    let round_robin_counter = Arc::new(AtomicUsize::new(0));
+
+    let mut handles = vec![];
+
+    for i in 0..producer_count {
+        let channels = channels.clone();
+        let round_robin_counter = Arc::clone(&round_robin_counter);
+        let should_exit = Arc::clone(&should_exit);
+        let handle = thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+            let mut msg_count = 0;
+            loop {
+                if msg_count % 10 == 0 && should_exit.load(Ordering::Relaxed) {
+                    break;
+                }
+
+                let len: usize = rng.gen_range(100..1000);
+                let msg = "a".repeat(len);
+                let index = round_robin_counter.fetch_add(1, Ordering::SeqCst) % consumer_count;
+                if let Err(_) = channels[index].0.send_timeout(msg, Duration::from_millis(10)) {
+                    break;
+                }
+                msg_count += 1;
+            }
+        });
+        handles.push(handle);
+    }
+
+    for i in 0..consumer_count {
+        let (_tx, rx) = channels[i].clone();
+        let counter = Arc::clone(&counters[i]);
+        let length = Arc::clone(&lengths[i]);
+        let should_exit = Arc::clone(&should_exit);
+        let handle = thread::spawn(move || {
+            consumer(rx.clone(), i, counter, length, should_exit);
+        });
+        handles.push(handle);
+    }
+
+    thread::sleep(Duration::from_secs(seconds));
+    should_exit.store(true, Ordering::SeqCst);
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    counters.iter().map(|counter| counter.load(Ordering::SeqCst)).sum()
 }
